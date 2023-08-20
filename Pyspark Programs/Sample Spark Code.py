8f5ce5c4bc02f27cf968a92a4ef12c86a6751b54
Apoorv <apoorv.mehrotra.am@gmail.com>
2023-08-20 11:25:39 +0530

    Adding Sample Spark Code

--- Pyspark Programs/Sample Spark Code.py
+++ Pyspark Programs/Sample Spark Code.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("Sample Spark Program").getOrCreate()
-print(spark)
+df = spark.read.csv("Learning/data/the_global_k_anon_dataset.csv", inferSchema=True, header=True)
+df.select([ "Year", "Case_Category"]).show()
