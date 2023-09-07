26321b1c4c9bfc67f8a151ed7a0bc22ee4a508c0
Apoorv <apoorv.mehrotra.am@gmail.com>
2023-09-07 15:38:44 +0530

    Added Data and exception handling

--- Pyspark Programs/Sample Spark Code.py
+++ Pyspark Programs/Sample Spark Code.py
@@ -1,5 +1,8 @@
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("Sample Spark Program").getOrCreate()
-df = spark.read.csv("Learning/data/the_global_k_anon_dataset.csv", inferSchema=True, header=True)
-df.select([ "Year", "Case_Category"]).show()
+try:
+    df = spark.read.csv("sample project/Learning/data/the_global_k_anon_dataset.csv", inferSchema=True, header=True)
+    df.show()
+except Exception as err:
+    print(err)
